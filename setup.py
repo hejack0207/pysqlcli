18f84544b3cc44c59e3d68affd69939ae27f14ee
hejack0207 <hejack0207@sina.com>
2017-09-15 17:03:02 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pysqlcli',
+      version='1.0',
+      description='Python oracle client',
+      author='hejack0207',
+      author_email='hejack0207@sina.com',
+      url='https://github.com/hejack0207/pysqlcli',
+      packages=['distutils', 'distutils.command'],
+     )

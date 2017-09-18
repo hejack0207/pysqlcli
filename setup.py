49cc795fabc5dc20095849fc8893ab56b68652ac
hejack0207 <hejack0207@sina.com>
2017-09-18 16:22:50 +0800

    complete setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,19 @@
 
 from distutils.core import setup
 
+install_requires = [
+    'click=6.7',
+    'cx_Oracle=6.0.2',
+    ]
+
 setup(name='pysqlcli',
       version='1.0',
       description='Python oracle client',
       author='hejack0207',
       author_email='hejack0207@sina.com',
       url='https://github.com/hejack0207/pysqlcli',
-      packages=['distutils', 'distutils.command'],
+      packages=['pysqlcli'],
+      package_dir = {'': 'lib'},
+      install_requires=install_requires,
+      scripts=['bin/pysqlcli'],
      )

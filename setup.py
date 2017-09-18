175869f50e91e235e98933c44f31b8734b3c50ad
hejack0207 <hejack0207@sina.com>
2017-09-18 17:13:08 +0800

    fix error

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
 #from distutils.core import setup
-from setuptools import setup
+from setuptools import setup, find_packages
 
 install_requires = [
     'click==6.7', 'cx_Oracle==6.0.2'
     ]
 
 setup(name='pysqlcli',
-      version='1.0',
+      version='1.1',
       description='Python oracle client',
       author='hejack0207',
       author_email='hejack0207@sina.com',
       url='https://github.com/hejack0207/pysqlcli',
-      packages=['pysqlcli'],
+      #packages=['pysqlcli'],
       package_dir = {'': 'lib'},
+      packages=find_packages('lib'),
       install_requires=install_requires,
       scripts=['bin/pysqlcli'],
      )
